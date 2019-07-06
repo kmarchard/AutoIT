9fd97152bd94ca70e47d8b2e94cb22e16b7d14d2
kmarchard <krish6898@gmail.com>
2019-07-06 11:27:28 +0530

    initial add

---
+++ src/test/java/AutoITFileUpload.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class AutoITFileUpload {
+@Test
+public void fileUpload() throws InterruptedException, IOException{
+	System.getProperty("Webdriver.chrome.driver", "C:\\Users\\bhoomi\\workspace\\AutoItArt\\chromedriver.exe");
+	DesiredCapabilities capabilities = new DesiredCapabilities();
+	capabilities.setCapability("something", true);
+	
+	WebDriver dr=new ChromeDriver(capabilities);
+	dr.manage().window().maximize();
+	dr.get("file:///C:/Users/bhoomi/Desktop/upload/fileupload.html");
+	dr.findElement(By.xpath("//*[@id='1']")).click();
+	Thread.sleep(2000);
+	Runtime.getRuntime().exec("C:\\Users\\bhoomi\\Desktop\\upload\\upload.exe");
+}
+
+}
